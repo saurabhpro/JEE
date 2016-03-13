0f5bfe1bcd031936d0bf584c1f6197e03d20a559
Saurabh Kumar <saurabhk1511@gmail.com>
2016-03-13 21:20:26 +0530

    Image store not working! TODO overwriting exisiting named file

--- UploadDownload/src/ExcelCreateDownloadServlet.java
+++ UploadDownload/src/ExcelCreateDownloadServlet.java
@@ -10,7 +10,7 @@ import java.io.PrintWriter;
  * Created by Saurabh on 3/13/2016.
  * * downloads at default location of your browser
  */
-@WebServlet(name = "ExcelCreateDownloadServlet",urlPatterns = {"/excel"})
+@WebServlet(name = "ExcelCreateDownloadServlet", urlPatterns = {"/excel"})
 public class ExcelCreateDownloadServlet extends HttpServlet {
 
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- UploadDownload/web/index.jsp
+++ UploadDownload/web/index.jsp
@@ -7,10 +7,14 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
+<head>
     <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+</head>
+<body>
+$END$
+<form action="finUpload" enctype="multipart/form-data" method="post">
+    <input type="file" name="file">
+    <input type="submit">
+</form>
+</body>
 </html>
