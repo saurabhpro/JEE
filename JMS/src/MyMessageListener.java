9a350686cc731f8c420de79e36510db39b64d296
Saurabh Kumar <saurabhk1511@gmail.com>
2016-04-03 02:04:48 +0530

    Session Traacking with cookies Urlrewriting hidden form feild

---
+++ JMS/src/MyMessageListener.java
@@ -0,0 +1,25 @@
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * Created by Saurabh on 4/2/2016.
+ */
+public class MyMessageListener implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+        try {
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+
+                String string = textMessage.getText();
+                System.out.println(string);
+            } else {
+                System.out.println("Message in Unknown Format");
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
